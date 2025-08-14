0f9374cec55c112e78e6e22bb812d3d0aa1cabca
Josh9Huang <90446121+Josh9Huang@users.noreply.github.com>
2025-08-14 13:42:00 +0800

    Add files via upload
    init

---
+++ src/main/java/com/example/csvjsonprocessor/CsvJsonProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.csvjsonprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvJsonProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvJsonProcessorApplication.class, args);
+	}
+
+}
